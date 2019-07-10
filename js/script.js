847ab57a75d4d8dc68d25837abaef804c9e26ccd
Tomek Palgan <tompal.tp81@gmail.com>
2019-07-10 19:43:03 +0100

    Add class 'active' to clicked link

--- js/script.js
+++ js/script.js
@@ -4,30 +4,36 @@
 });     */
 
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log(event);
+	const clickedElement = this;	
+	console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
+  	/* [DONE] remove class 'active' from all article links  */
+	
+	const activeLinks = document.querySelectorAll('.titles a.active');
 
-	for(let activeLink of activeLinks){
-	  activeLink.classList.remove('active');
-}
+		for(let activeLink of activeLinks){
+		  activeLink.classList.remove('active');
+	}
 
-  /* add class 'active' to the clicked link */
+  	/* [DONE] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('article');
+  	clickedElement.classList.add('active');
+  	
+  	console.log('clickedElement:', clickedElement);
+  	
+  	/* [DONE] remove class 'active' from all articles */
+	
+	const activeArticles = document.querySelectorAll('article');
 
-	for(let activeArticle of activeArticles){
-	  activeArticle.classList.remove('active');
-}
+		for(let activeArticle of activeArticles){
+		  activeArticle.classList.remove('active');
+	}
 
-  /* get 'href' attribute from the clicked link */
+  	/* get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  	/* find the correct article using the selector (value of 'href' attribute) */
 
-  /* add class 'active' to the correct article */
+  	/* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
