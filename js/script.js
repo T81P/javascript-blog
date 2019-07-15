d964d2b68d0d289abc5077f33a071b3454622e04
Tomek Palgan <tompal.tp81@gmail.com>
2019-07-15 13:18:33 +0100

    Generating a list of titles

--- js/script.js
+++ js/script.js
@@ -52,3 +52,52 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+{
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+  function generateTitleLinks(){
+
+    /* remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+
+      let html = '';
+      for(let article of articles){
+
+      /* get the article id */
+
+      const articleId = document.getAttribute('id');
+
+
+      /* find the title element */
+
+      /* get the title from the title element */
+
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* create HTML of the link */
+
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      
+
+      /* insert link into titleList */
+
+      html = html + linkHTML;
+
+      }
+
+      titleList.innerHTML = html;
+  }
+
+
+  generateTitleLinks();
+}
